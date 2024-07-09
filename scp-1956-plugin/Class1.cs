fd7b60f488f4989f33e51f9e0dd57ea49ac6a13c
Samirs Vertahovs <samirs.vertahovs@gmail.com>
2024-07-09 12:03:41 +0200

    Add project files.

---
+++ scp-1956-plugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scp_1956_plugin
+{
+    public class Class1
+    {
+    }
+}
